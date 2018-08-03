e0be0f15e18bda2cd4c987c0dbf70046509c834e
Shariff Mazlan <shariffmazlan@MacAdmins-MBP-2.fios-router.home>
2018-08-02 21:04:18 -0400

    initial java commit

---
+++ src/Rectangle.java
@@ -0,0 +1,17 @@
+public class Rectangle {
+	public int length;
+	public int width;
+
+	public Rectangle(int length, int width) {
+		this.length = length;
+		this.width = width;
+	}
+
+	public int getArea() {
+		return length * width;
+	}
+
+	public int gerPerimeter() {
+		return 2 * (length + width)
+	}
+}
