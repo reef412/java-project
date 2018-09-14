be5dc9e6826d5fc20bc7f56604c86f0ed3ebcd68
Shariff Mazlan <shariffmazlan@MacAdmins-MacBook-Pro-2.local>
2018-09-13 21:17:00 -0400

    Changed path

--- mathlib.py
+++ mathlib.py
@@ -3,9 +3,3 @@ def calc_total (a,b):
 
 def calc_multiply (a,b):
 	return a * b
-
-<<<<<<< HEAD
-echo "Hello World"
-=======
-print("Goodbye, World!")
->>>>>>> 2cb82b20b7cc2f67a6413d920f1965bdc0875c91
